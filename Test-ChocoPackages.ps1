36cc54c33bdb3b964a875b202a42bedce72f89f4
ripclawffb <ripclaw_ffb@hotmail.com>
2019-12-11 20:39:09 -0800

    Add comments

--- Test-ChocoPackages.ps1
+++ Test-ChocoPackages.ps1
@@ -12,6 +12,10 @@ function Test-ChocoPackages {
     }
 
     END {
+        # This will let appveyor fail the build if a non-zero exit code is
+        # returned by any chocolatey package
+        #
+        # Reference: https://www.appveyor.com/docs/build-configuration/
         if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
     }
 }
